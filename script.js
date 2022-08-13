f301a64c3d3ff4bfa1596def6b05d43032cd85c4
Pascal Steffen <pascalsteffenbusiness@hotmail.com>
2022-08-13 15:54:16 +0200

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let photos = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg", "img/7.jpg", "img/8.jpg", "img/9.jpg", "img/10.jpg", "img/11.jpg", "img/12.jpg"];
 let slide = 0;
 
+
+/**
+ * render all images.
+ * 
+ */
 function render() {
     let mainBodyArea = document.getElementById("mainBody");
     mainBodyArea.innerHTML = "";
@@ -13,6 +18,12 @@ function render() {
     }
 }
 
+
+/**
+ * zoom in the current image.
+ * @param {number} i 
+ * 
+ */
 function openImage(i) {
     document.getElementById("open").classList.remove("dNone");
 
@@ -24,14 +35,22 @@ function openImage(i) {
             <img class="mainImage" id="imageSlide"src="${photos[i]}" alt="">
             <img class="arrowRight" onclick="imageRight(${i})" src="img/arrow-right.png" alt="">
         </div>`;
-
-
 }
 
+
+/**
+ * close the current image.
+ * 
+ */
 function closeImage() {
     document.getElementById("open").classList.add("dNone");
 }
 
+
+/**
+ * image slide to right.
+ * 
+ */
 function imageRight() {
     slide += 1;
     if (slide >= photos.length) {
@@ -40,6 +59,11 @@ function imageRight() {
     document.getElementById("imageSlide").src = photos[slide];
 }
 
+
+/**
+ * image slide to left.
+ * 
+ */
 function imageLeft() {
     slide -= 1;
     if (slide >= photos.length)
