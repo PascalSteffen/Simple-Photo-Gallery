eaee8794a877436ff02e84375b247b4c16b53c1f
Pascal Steffen <pascalsteffenbusiness@hotmail.com>
2022-08-13 15:51:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let photos = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg", "img/7.jpg", "img/8.jpg", "img/9.jpg", "img/10.jpg", "img/11.jpg", "img/12.jpg"];
+let slide = 0;
+
+function render() {
+    let mainBodyArea = document.getElementById("mainBody");
+    mainBodyArea.innerHTML = "";
+
+    for (let i = 0; i < photos.length; i++) {
+        const photoGalla = photos[i];
+
+        mainBodyArea.innerHTML += /*html*/ `
+        <img onclick="openImage(${i})" src="${photoGalla}" alt="">`;
+    }
+}
+
+function openImage(i) {
+    document.getElementById("open").classList.remove("dNone");
+
+    let showImage = document.getElementById("showImage");
+    showImage.innerHTML = /*html*/ `
+        <img class="closeIcon" onclick="closeImage()" src="img/x-mark.png" alt="">
+        <div class="flex">
+            <img class="arrowLeft" onclick="imageLeft(${i})"src="img/arrow-left.png" alt="">
+            <img class="mainImage" id="imageSlide"src="${photos[i]}" alt="">
+            <img class="arrowRight" onclick="imageRight(${i})" src="img/arrow-right.png" alt="">
+        </div>`;
+
+
+}
+
+function closeImage() {
+    document.getElementById("open").classList.add("dNone");
+}
+
+function imageRight() {
+    slide += 1;
+    if (slide >= photos.length) {
+        slide = 0;
+    }
+    document.getElementById("imageSlide").src = photos[slide];
+}
+
+function imageLeft() {
+    slide -= 1;
+    if (slide >= photos.length)
+        slide = 0;
+    if (slide < 0)
+        slide = photos.length - 1;
+    document.getElementById("imageSlide").src = photos[slide];
+}
